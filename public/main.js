736803fa66331cd9cc39f70d9b88eb46290ad779
Jimmy Li <lijjimmy@gmail.com>
2021-04-05 22:53:16 -0400

    added rest of the CRUD methods

--- public/main.js
+++ public/main.js
@@ -1,13 +1,79 @@
 
+const deleteBtn = document.querySelectorAll('.del')
 
-const update = document.querySelector('#update')
+const todoItem = document.querySelectorAll('.todoItem span')
 
-update.addEventListener('click', _ => {
-    fetch('/chores', {
-        method: 'put',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            'choreName': 'take trash out'
-        })
-    })
+const markedComplete = document.querySelectorAll('.todoItem span.completed')
+
+
+Array.from(deleteBtn).forEach((element) => {
+    element.addEventListener('click', deleteToDo)
 })
+
+Array.from(todoItem).forEach((element) => {
+    element.addEventListener('click', markComplete)
+})
+
+
+Array.from(markedComplete).forEach((element) => {
+    element.addEventListener('click', undo)
+})
+
+
+async function deleteToDo() {
+    const todoText = this.parentNode.childNodes[1].innerText
+    try {
+        const response = await fetch('deleteTodo', {
+            method: 'delete',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'rainbowUnicorn': todoText
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+
+async function markComplete() {
+    const todoText = this.parentNode.childNodes[1].innerText
+    try {
+        const response = await fetch('markComplete', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'rainbowUnicorn': todoText
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+
+
+
+async function undo() {
+    const todoText = this.parentNode.childNodes[1].innerText
+    try {
+        const response = await fetch('undo', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'rainbowUnicorn': todoText
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    } catch(err) {
+        console.log(err)
+    }
+}
--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-const http = require('http')
+
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 8000
+require('dotenv').config()
 
-
-app.use(bodyParser.urlencoded({ extended: true }))
 
 let db
-let dbConnectionStr = 'mongodb+srv://Jimmy:demo@cluster0.k3eyb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+let dbConnectionStr = process.env.DB_STRING
 let dbName = 'To-Do-List'
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
 .then(client => {
     console.log(`Connected to ${dbName} Database`)
-    db = client.db(dbName) // Database name
-    const toDoCollection = db.collection('todo-list')   // Collection name
+    db = client.db(dbName) // collection name
     
 })
 .catch(error => console.log(error))
@@ -27,35 +24,76 @@ app.set('view engine', 'ejs') // Has to go before any app.use, app.get, or app.p
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(bodyParser.json())
 
-app.get('/', (request, response) => {
-    console.log("Why you get")
-    db.collection('todo-list').find().toArray() // Gives us all items in our db
-    .then(item => {
-        console.log(item)
-        response.render('index.ejs', {chores: item})
-    })
-    .catch(error => console.log(error))
+app.get('/', async (request, response) => {
+    // try {
+        const todoItems = await db.collection('todo-list').find().toArray()
+        const itemsLeft = await db.collection('todo-list').countDocuments({completed: false})
+        response.render('index.ejs', {chores: todoItems, left: itemsLeft})
+    // } catch(err) {
+    //     console.log(err)
+    // }
     
-    // response.sendFile(__dirname + '/index.html')
+    // db.collection('todo-list').find().toArray() // Gives us all items in our db
+    // .then(item => {
+    //     response.render('index.ejs', {chores: item})
+    // })
+    // .catch(error => console.log(error))
+
 })
 
 app.post('/addItem', (request, response) => {
     console.log(request.body)
-    db.collection('todo-list').insertOne(request.body)
+    db.collection('todo-list').insertOne({todo: request.body.ToDoItem, completed: false})
     .then(result => {
-        console.log(result)
+        console.log(`Added ${result}`)
         response.redirect('/')
     })
     .catch(error => console.log(error))
 })
 
-app.put('/chores', (request, response) => {
+app.put('/markComplete', (request, response) => {
     console.log(response.body)
-    db.collection('todo-list').findOutAndUpdate(
-        
-    )
+    db.collection('todo-list').updateOne( {todo: request.body.rainbowUnicorn}, {
+        $set: {
+            completed: true
+        }
+    })
+    .then(result => {
+        console.log('Makred Complete')
+        response.json('Marked Complete')
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+
+app.put('/undo', (request, response) => {
+    db.collection('todo-list').updateOne( {todo: request.body.rainbowUnicorn}, {
+        $set: {
+            completed: false
+        }
+    })
+    .then(result => {
+        console.log('Marked Complete')
+        response.json('Marked Complete')
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+
+app.delete('/deleteTodo', (request, response) => {
+    console.log(request.body.rainbowUnicorn)
+    db.collection('todo-list').deleteOne( {todo: request.body.rainbowUnicorn})
+    .then(result => {
+        console.log('Deleted Todo Item')
+        response.json('Delete it')
+
+    })
+    .catch(error => {
+        console.log(error)
+    })
 })
 
 app.listen(process.env.PORT || PORT, ()=> {
